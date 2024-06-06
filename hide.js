3b940308fc421d9877cccde14e0a33f0a5ac7bc0
CACLR <136112842+CACLR@users.noreply.github.com>
2024-06-06 12:22:55 +0200

    Create hide.js

---
+++ hide.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const element = document.querySelector(".chat-header-settings-button.css-1f6wjz1");
+    if (element) {
+        element.style.display = "none";
+    }
+});
